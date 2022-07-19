e6d19d9be743c585b64e4e1da1f71f9147e687c4
cybertilla <bertilla.fabris@gmail.com>
2022-07-19 17:04:34 +0200

    vue right version startup and API fetch

---
+++ poetrysite-shiny/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <header>
+<NavBar></NavBar>
+  </header>
+
+  <main>
+      <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ poetrysite-shiny/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script>
+  import { MDBBtn, MDBNavbar, MDBNavbarBrand } from 'mdb-vue-ui-kit';
+  export default {
+    components: {
+      MDBBtn,
+      MDBNavbar,
+      MDBNavbarBrand,
+    },
+  };
+</script>
+
+<template>
+  <MDBNavbar light bg="light" container>
+    <MDBNavbarBrand href="/">Poetry Search</MDBNavbarBrand>
+    <MDBBtn>Authors</MDBBtn>
+    <MDBBtn>Random</MDBBtn>
+  </MDBNavbar>
+</template>
