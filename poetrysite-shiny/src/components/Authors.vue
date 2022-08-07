f1e10d566fa6cbdf325ba085d370e2f7ed1ef33f
cybertilla <bertilla.fabris@gmail.com>
2022-08-07 09:48:29 +0200

    list of authors as buttons

--- poetrysite-shiny/src/components/Authors.vue
+++ poetrysite-shiny/src/components/Authors.vue
@@ -1,31 +1,20 @@
 <script>
 import axios from 'axios';
 import Modal from './Modal.vue'
-  import { MDBModal, MDBModalHeader, MDBModalTitle, MDBModalBody, MDBModalFooter, MDBBtn } from "mdb-vue-ui-kit";
-  import { ref } from 'vue';
+// import { MDBModal, MDBModalHeader, MDBModalTitle, MDBModalBody, MDBModalFooter, MDBBtn } from "mdb-vue-ui-kit";
 
 export default {
-    components: {
-      MDBModal,
-      MDBModalHeader,
-      MDBModalTitle,
-      MDBModalBody,
-      MDBModalFooter,
-      MDBBtn
-    },
-    setup() {
-      const varyingExampleModal = ref(false);
-      const modalContent = ref('');
-      return {
-        varyingExampleModal,
-        modalContent,
-      }
-    }
-  ,
+  components: {
+    Modal
+  },
   data: () => ({
     authors: ['Loading...'],
-    header: "List of authors"
-          }),
+    header: "List of authors",
+
+    modalVisible: true,
+    modalData: "string"
+
+  }),
   created() {
     // fetch on init
     this.fetchData()
@@ -35,17 +24,31 @@ export default {
     async fetchData() {
       const response = await axios.get('https://poetrydb.org/author');
       this.authors = response.data.authors;
+      console.log(this.header);
+    },
+
+    openModal(author) {
+      this.modalData = author
+      this.modalVisible = true
     }
+
   }
 };
 </script>
 
 
 <template>
-
-<!--
-
   <h3>{{ header }}</h3>
+  
+    <div>
+      <Modal v-if="modalVisible" @close="modalVisible = false" :data="modalData" />
+
+      <template v-for="author in authors">
+        <button type="button" @click="openModal(author)"> {{ author }}</button>
+      </template>
+    </div>
+
+  <!--
 
   <template v-for="author in authors">
     <ul>
@@ -53,52 +56,4 @@ export default {
     </ul>
   </template>
 -->
-
-  <MDBBtn
-    color="primary"
-    aria-controls="varyingExampleModal"
-    @click="
-      () => {
-        modalContent = 'author1';
-        varyingExampleModal = true;
-      }
-    "
-  >
-    author11
-  </MDBBtn>
-
-  
-  <MDBModal
-    id="varyingExampleModal"
-    tabindex="-1"
-    labelledby="varyingExampleModalLabel"
-    v-model="varyingExampleModal"
-  >
- 
-    <MDBModalBody>
-
-      
-      <form>
-        <div class="mb-3">
-          <label for="recipient-name">Recipient: </label>
-          <input
-            type="text"
-            class="form-control"
-            id="recipient-name"
-            :value="modalContent"
-          />
-
-
-        </div>
-
-      </form>
-    </MDBModalBody>
-
-
-    <MDBModalFooter>
-      <MDBBtn color="secondary" @click="varyingExampleModal = false"> Close </MDBBtn>
-    </MDBModalFooter>
-  </MDBModal>
-
-
 </template>
